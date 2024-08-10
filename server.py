2cda122cc77eb4961027b098a62f421461ac4ae4
zamuel24 <emanuelzacari24@gmail.com>
2024-08-10 23:54:51 +0000

    Mundo guardado 2024-08-10

--- server.py
+++ server.py
@@ -16,10 +16,7 @@
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
-
-
-
-# No toques nada de aquí para abajo, puedes dañarlo
+# No toques nada de aquí para abajo, puedes dañarlo2
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
